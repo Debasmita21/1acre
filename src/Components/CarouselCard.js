49c5c62248aa54407941b008308b6679a3a52265
Debasmita21 <63196707+Debasmita21@users.noreply.github.com>
2024-04-12 19:05:42 +0530

    updated readme file

--- src/Components/CarouselCard.js
+++ src/Components/CarouselCard.js
@@ -1,7 +1,5 @@
 import React from "react";
-//import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
